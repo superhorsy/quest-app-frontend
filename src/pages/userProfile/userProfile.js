f361f9e805e37ca6b445594f55a448395d5adf0d
Irina Adamuk <grinirina032@gmail.com>
2022-11-27 23:54:59 +0200

    profile review

--- src/pages/userProfile/userProfile.js
+++ src/pages/userProfile/userProfile.js
@@ -1,78 +1,125 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import avatarLogo from '../../assets/images/avatar-icon.jpg'
-
+import CreateIcon from "@mui/icons-material/Create";
+import avatarLogo from "../../assets/images/avatar-icon.jpg";
 
 export const UserProfile = () => {
-
-  const [email, setEmail] = useState('');
-  const [nickname, setNickname] = useState('');
+  const [email, setEmail] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [isEditProfile, setIsEditProfile] = useState(false);
 
   const avatar = avatarLogo;
-  
-  const sendData = (event) => {
+  const navigate = useNavigate();
+
+  const handleEditProfile = () => {
+    setIsEditProfile(true);
+  };
+
+  const handleSubmitEditProfile = (event) => {
     event.preventDefault();
-  }
+  };
 
   const isEmptyField = !email || !nickname;
 
   return (
     <div className="page-container">
       <div className="main-container">
-
         <h1 className="title">Мой Профиль</h1>
-        <img src = {avatar} className="avatar-logo" alt="avatar" />
+        <img src={avatar} className="avatar-logo" alt="avatar" />
 
         <Box
-          component="form"
+          component="div"
           sx={{
-            m: '0 auto',
+            m: "0 auto",
             textAlign: "center",
-            width: {xs: 1 / 1, sm: 500},
+            width: { xs: 1 / 1, sm: 500 },
           }}
-          noValidate={false}
-          autoComplete="off"
-          onSubmit={sendData}
         >
-          <div>
-            <TextField
-              required
-              fullWidth
-              sx={{mb: {xs: 3, sm: 4}}}
-              id="outlined-basic"
-              label="Ваш Никнейм"
-              variant="outlined"
-              value={nickname}
-              onChange={(e) => setNickname(e.target.value)}
-            />
-            <TextField
-              required
-              fullWidth
-              sx={{mb: {xs: 3, sm: 4}}}
-              id="outlined-basic"
-              type="Ваш email"
-              label="Ваш email"
-              variant="outlined"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+          <Box
+            component="form"
+            sx={{
+              m: "0 auto",
+              textAlign: "center",
+              width: { xs: 1 / 1, sm: 500 },
+            }}
+            noValidate={false}
+            autoComplete="off"
+            onSubmit={handleSubmitEditProfile}
+          >
             <div>
-              <Button
+              <TextField
+                disabled={!isEditProfile}
+                required
                 fullWidth
-                type="submit"
-                sx={{mb: {xs: 4, sm: 6}}}
-                variant="contained"
-                size="large"
-                disabled={isEmptyField}
-              >Изменить пароль
-              </Button>
+                sx={{ mb: { xs: 3, sm: 4 } }}
+                id="outlined-basic"
+                label="Ваш Никнейм"
+                variant="outlined"
+                value={nickname}
+                onChange={(e) => setNickname(e.target.value)}
+              />
+              <TextField
+                disabled={!isEditProfile}
+                required
+                fullWidth
+                sx={{ mb: { xs: 3, sm: 4 } }}
+                id="outlined-basic"
+                type="Ваш email"
+                label="Ваш email"
+                variant="outlined"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <div>
+                {!isEditProfile && (
+                  <Button
+                    fullWidth
+                    startIcon={<CreateIcon />}
+                    sx={{ mb: { xs: 4, sm: 6 } }}
+                    variant="contained"
+                    size="large"
+                    onClick={handleEditProfile}
+                  >
+                    Редактировать
+                  </Button>
+                )}
+
+                {isEditProfile && (
+                  <Button
+                    fullWidth
+                    type="submit"
+                    sx={{ mb: { xs: 4, sm: 6 } }}
+                    variant="contained"
+                    size="large"
+                    disabled={isEmptyField}
+                  >
+                    Сохранить
+                  </Button>
+                )}
+              </div>
             </div>
+          </Box>
+
+          <div>
+            <Button
+              fullWidth
+              sx={{mt: 5, mb: { xs: 4, sm: 6 } }}
+              variant="contained"
+              size="large"
+              disabled={isEmptyField}
+              onClick={() => {
+                // должна быть ссылка на страницу изменения пароля
+                navigate("/");
+              }}
+            >
+              Изменить пароль
+            </Button>
           </div>
         </Box>
       </div>
     </div>
   );
 };
-    
