91de289a4a6cf5cf23bd2fa0872e4aed26de6fb3
isakhalin <47768954+isakhalin@users.noreply.github.com>
2022-12-05 01:45:07 +1100

    fix(modalRestorePass): Добавлено размытие фона в стили

--- src/components/modalResorePass/modalRestorePass.js
+++ src/components/modalResorePass/modalRestorePass.js
@@ -6,20 +6,22 @@ import Modal from '@mui/material/Modal';
 import TextField from "@mui/material/TextField";
 
 // Api
-import {userProfileApi} from "../../api/api"
+import {userProfileApi} from "../../api/api";
 
 const style = {
   position: 'absolute',
-  top: '50%',
+  top: '30%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: {xs: 300, md: 400},
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  border: '1px solid #000',
+  borderRadius: '5px',
   boxShadow: 24,
   p: 4,
   textAlign: 'center',
   fontSize: {xs: "14px", md: "20px"},
+  transition: 'transform 3s'
 };
 
 export const ModalRestorePass = (children) => {
@@ -59,6 +61,10 @@ export const ModalRestorePass = (children) => {
       <Modal
         open={open}
         onClose={handleClose}
+        slotProps={{backdrop: {timeout: 500, transition: 'backdropFilter 5s'} }}
+        sx={{
+          backdropFilter: 'blur(5px)',
+        }}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
