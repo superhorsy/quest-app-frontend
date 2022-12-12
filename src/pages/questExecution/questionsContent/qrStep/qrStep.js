1bf10feb232b57dd2a63574ebf5e007f06b27e56
isakhalin <47768954+isakhalin@users.noreply.github.com>
2022-12-12 17:54:28 +1100

    new files added

---
+++ src/pages/questExecution/questionsContent/qrStep/qrStep.js
@@ -0,0 +1,27 @@
+import React from "react";
+import QRScan from "../../../../components/qrCodeReader/qrCodeReader";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+
+export const QRStep = ({ qrCodeAnswer }) => {
+  const { success } = useSelector((state) => state.questExecutionReducer);
+  const [toggleScanQR, setToggleScanQR] = useState(true);
+  useEffect(() => {
+    if (!success) {
+      setToggleScanQR(true);
+    }
+  }, []);
+  return (
+    <>
+      {qrCodeAnswer && <Typography variant="h6" sx={{textAlign: "center"}}>{qrCodeAnswer}</Typography>}
+      {!toggleScanQR && !qrCodeAnswer && <QRScan success={success} />}
+      {toggleScanQR && (
+        <Button onClick={() => setToggleScanQR(false)}>
+          Отсканировать QR-код
+        </Button>
+      )}
+    </>
+  );
+};
