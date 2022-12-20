37ee43ea317818b96fbfa77bb301f1bfdde41975
Irina Adamuk <grinirina032@gmail.com>
2022-12-20 19:25:55 +0200

    useless cpmponents seleted userQuestList fixed

--- src/pages/questExecution/newStepper/newStepper.js
+++ src/pages/questExecution/newStepper/newStepper.js
@@ -166,7 +166,7 @@ export const QuestExecution = () => {
             <Box component="div" className={styles.imageBox}>
               {!matches && (
                 <img
-                  src={`${current.question_content}&w=500&h=500&fit=contain`}
+                  src={`${current.question_content}&w=450&h=450&fit=contain`}
                   alt="задание с картинкой"
                 />
               )}
